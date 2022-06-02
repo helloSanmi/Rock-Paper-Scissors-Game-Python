0e0c5813baf95861620ef91e5e8924244e4d694d
Oluwasanmi Idowu <95680393+helloSanmi@users.noreply.github.com>
2022-06-02 14:34:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 
 
+# function to determine the winner
 def determine_winner(player, cpu):
     if player == cpu:
         print(f'Both players selected {player}. It\'s a tie!')
@@ -21,6 +22,7 @@ def determine_winner(player, cpu):
             print("Rock smashes scissors! You lose!")
 
 
+# loop to play the game, loop ends when user select No
 while True:
     while True:
         try:
@@ -37,19 +39,16 @@ while True:
     possible_actions = ["R", "P", "S"]
     computer_action = random.choice(possible_actions)
 
-    # print(f"\nYou chose {user_input}, Computer chose {computer_action}. It\'s is a tie. \n")
     user_input = user_input.upper()
     computer_action = computer_action.upper()
 
     determine_winner(user_input, computer_action)
 
     play_again = input("Play again? ('Y' for Yes/'N' for No): ")
+    
     if play_again.lower() not in ('y', 'n'):
         continue
     else:
         if play_again.lower() != "y":
             print("Thank you for playing")
             break
-
-
-
