78a7e61e0161fa599b493250dae49e206cb764d3
Oluwasanmi Idowu <95680393+helloSanmi@users.noreply.github.com>
2022-06-02 14:32:15 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+import random
+
+
+def determine_winner(player, cpu):
+    if player == cpu:
+        print(f'Both players selected {player}. It\'s a tie!')
+    elif player == "R":
+        if cpu == "S":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose!")
+    elif player == "P":
+        if cpu == "R":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose!")
+    elif player == "S":
+        if cpu == "P":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose!")
+
+
+while True:
+    while True:
+        try:
+            user_input = input('Enter a choice ("R" for "rock", "P" for "paper", "S" for "scissors"): ')
+            if user_input.upper() not in ('R', 'P', 'S'):
+                print('Wrong input, try again')
+            else:
+                break
+
+        except ValueError:
+            print('Wrong input, try again')
+            continue
+
+    possible_actions = ["R", "P", "S"]
+    computer_action = random.choice(possible_actions)
+
+    # print(f"\nYou chose {user_input}, Computer chose {computer_action}. It\'s is a tie. \n")
+    user_input = user_input.upper()
+    computer_action = computer_action.upper()
+
+    determine_winner(user_input, computer_action)
+
+    play_again = input("Play again? ('Y' for Yes/'N' for No): ")
+    if play_again.lower() not in ('y', 'n'):
+        continue
+    else:
+        if play_again.lower() != "y":
+            print("Thank you for playing")
+            break
+
+
+
